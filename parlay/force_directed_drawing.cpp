eee068bc41cf9d8ba0fc55942f0ad17915a69d6f
Shubham Bhargava <shubhargava@gmail.com>
2024-04-30 17:19:15 +0000

    refactored

---
+++ parlay/force_directed_drawing.cpp
@@ -0,0 +1,48 @@
+
+#include <iostream>
+#include <string>
+
+#include "graph_utils.h"
+#include "parlay/internal/get_time.h"
+#include "parlay/primitives.h"
+
+#include "force_directed_drawing.h"
+
+int main(int argc, char *argv[])
+{
+    auto usage = "Usage: calculate_pos <filename>";
+    if (argc != 2)
+    {
+        std::cout << usage << std::endl;
+        exit(1);
+    }
+
+    auto str = parlay::file_map(argv[1]);
+    auto tokens = parlay::tokens(str);
+    uint n = parlay::chars_to_uint(tokens[0]);
+    uint m = parlay::chars_to_uint(tokens[1]);
+    if (tokens.size() != m * 2 + 2)
+    {
+        std::cout << "bad file format" << std::endl;
+        exit(1);
+    }
+    parlay::sequence<edge> E;
+
+    for (uint i = 0; i < m; i++)
+    {
+        int u = parlay::chars_to_int(tokens[2 * i + 2]);
+        int v = parlay::chars_to_int(tokens[2 * i + 3]);
+        E.push_back(std::make_pair(u, v));
+    }
+
+    auto positions = calculate_pos(E, n, 1000);
+    std::cout << n << " " << m << std::endl;
+    for (coord &c : positions)
+    {
+        std::cout << std::get<0>(c) << " " << std::get<1>(c) << std::endl;
+    }
+    for (edge &e : E)
+    {
+        std::cout << e.first << " " << e.second << std::endl;
+    }
+}
---
+++ parlay/force_directed_drawing.h
@@ -0,0 +1,189 @@
+#ifndef FORCE_DIRECTED_DRAWING_H_
+#define FORCE_DIRECTED_DRAWING_H_
+
+#include <cmath>
+
+#include "parlay/primitives.h"
+#include "parlay/sequence.h"
+#include "parlay/random.h"
+
+using real = float;
+using edge = std::pair<int, int>;
+using coord = std::pair<real, real>;
+
+auto norm(real x, real y)
+{
+    return std::sqrt(x * x + y * y);
+}
+
+// Node of a Barnes-Hut tree. Square bounding box can be computed traversing down tree
+struct bh_tree
+{
+    bh_tree *ne, *nw, *se, *sw;
+    uint count; // Always >= 1
+    coord center_of_mass;
+    real width;
+};
+
+// count >1 acts as tag for internal node. TODO
+// struct bh_tree_internal : bh_tree {
+//     bh_tree *ne, *nw, *se, *sw;
+// };
+
+parlay::type_allocator<bh_tree> bh_tree_allocator;
+
+// Recursively create quadtree for Barnes-Hut
+bh_tree *make_bh_tree(parlay::slice<coord *, coord *> nodes, real lo_x = 0.0, real hi_x = 1.0, real lo_y = 0.0, real hi_y = 1.0)
+{
+    if (nodes.size() == 0)
+    {
+        return nullptr;
+    }
+
+    bh_tree *node = bh_tree_allocator.alloc();
+    node->count = nodes.size();
+
+    // TODO do in recursive step?
+    node->center_of_mass.first = parlay::reduce(parlay::delayed_map(nodes, [](coord x) -> real
+                                                         { return x.first; })) /
+                      nodes.size();
+    node->center_of_mass.second = parlay::reduce(parlay::delayed_map(nodes, [](coord x) -> real
+                                                          { return x.second; })) /
+                       nodes.size();
+    node->width = hi_x - lo_x;
+    node->se = nullptr;
+    node->sw = nullptr;
+    node->ne = nullptr;
+    node->nw = nullptr;
+
+    if (nodes.size() == 1)
+    {
+        return node;
+    }
+
+    auto mid_x = (lo_x + hi_x) / 2;
+    auto mid_y = (lo_y + hi_y) / 2;
+    auto get_key = [&](coord &c) -> size_t
+    { return 2 * (c.first <= mid_x) + (c.second <= mid_y); };
+
+    // TODO in place?
+    auto [sorted, counts] = parlay::counting_sort(nodes, 4, get_key);
+
+    // Construct subtrees (in parallel if sufficiently large)
+    parlay::par_do_if(counts[4] > 50, [&]()
+                      { parlay::par_do_if(counts[2] > 50, [&]()
+                                          { node->se = make_bh_tree(sorted.cut(counts[0], counts[1]), mid_x, hi_x, mid_y, hi_y); }, [&]()
+                                          { node->ne = make_bh_tree(sorted.cut(counts[1], counts[2]), mid_x, hi_x, lo_y, mid_y); }); }, [&]()
+                      { parlay::par_do_if(counts[4] - counts[2] > 50, [&]()
+                                          { node->sw = make_bh_tree(sorted.cut(counts[2], counts[3]), lo_x, mid_x, mid_y, hi_y); }, [&]()
+                                          { node->nw = make_bh_tree(sorted.cut(counts[3], counts[4]), lo_x, mid_x, lo_y, mid_y); }); });
+    return node;
+}
+
+real theta = 0.5;
+
+coord query(bh_tree *tree, const coord& curr, real k)
+{
+    // Empty Node
+    if (tree == nullptr)
+    {
+        return {0.0, 0.0};
+    }
+
+    real dist_x = curr.first - tree->center_of_mass.first;
+    real dist_y = curr.second - tree->center_of_mass.second;
+    real dist = norm(dist_x, dist_y);
+
+    // Ignore queried node and any nodes coincidentally at the same point
+    if (dist == 0)
+    {
+        return {0.0, 0.0};
+    }
+
+    // If external node or far enough internal node, calculate force using center of mass
+    if (tree->count == 1 || tree->width / dist <= 0.5)
+    {
+        real for_x = dist_x * tree->count * (k / dist) * (k / dist);
+        real for_y = dist_y * tree->count * (k / dist) * (k / dist);
+        return {for_x, for_y};
+    }
+
+    // Otherwise, calculate total force from children
+    // Not parallelised as queries will already be done in parallel
+    auto [sex, sey] = query(tree->se, curr, k);
+    auto [nex, ney] = query(tree->ne, curr, k);
+    auto [nwx, nwy] = query(tree->nw, curr, k);
+    auto [swx, swy] = query(tree->sw, curr, k);
+    return {sex + nex + nwx + swx, sey + ney + nwy + swy};
+}
+
+void free_bh_tree(bh_tree *tree)
+{
+    if (tree == nullptr)
+    {
+        return;
+    }
+
+    // par_do?
+    free_bh_tree(tree->ne);
+    free_bh_tree(tree->nw);
+    free_bh_tree(tree->sw);
+    free_bh_tree(tree->se);
+    bh_tree_allocator.retire(tree);
+}
+
+
+auto update_pos(const parlay::sequence<edge>& E, parlay::sequence<coord>& coords, uint n, real temp) {
+    bh_tree* tree = make_bh_tree(coords.cut(0, n));
+    real k = 0.1 * std::sqrt(((real) 1) / n);
+
+
+    parlay::sequence<coord> displacement = parlay::map(coords, [&] (coord& c) {return query(tree, c, k);});
+    free_bh_tree(tree);
+
+    parlay::parallel_for(0, E.size(), [&] (size_t curr_e) {
+        auto i = E[curr_e].first;
+        auto j = E[curr_e].second;
+        auto dx = coords[j].first - coords[i].first;
+        auto dy = coords[j].second - coords[i].second;
+        auto dist = norm(dx, dy);
+        if (dist > 0) {
+            displacement[i].first += dx * (dist / k);
+            displacement[i].second += dy * (dist / k);
+            displacement[j].first -= dx * (dist / k);
+            displacement[j].second -= dy * (dist / k);
+        }
+    });
+
+    parlay::parallel_for(0, n, [&] (size_t i) {
+        auto curr_disp = displacement[i];
+        auto len = norm(curr_disp.first, curr_disp.second);
+        coords[i].first += curr_disp.first * std::min((real) 1, temp / len);
+        coords[i].first = 1 - std::abs(1 - std::abs(coords[i].first));
+        coords[i].second += curr_disp.second * std::min((real) 1, temp / len);
+        coords[i].second = 1 - std::abs(1 - std::abs(coords[i].second));
+
+    });
+}
+
+auto calculate_pos(const parlay::sequence<edge>& E, uint n, uint iters, real temp_init = 1, real cooling_rate = 0.99) {
+
+    parlay::random_generator gen(0);
+    std::uniform_real_distribution<real> dis(0.0,1.0);
+
+    // generate n random points in a unit square
+    auto coords = parlay::tabulate(n, [&] (int i) {
+      auto r = gen[i];
+      return std::make_pair(dis(r), dis(r));
+    });
+
+    real temp = temp_init;
+    for (uint i = 0; i < iters; i++) {
+        update_pos(E, coords, n, temp);
+        temp *= cooling_rate;
+    }
+    
+    return coords;
+}
+
+#endif
